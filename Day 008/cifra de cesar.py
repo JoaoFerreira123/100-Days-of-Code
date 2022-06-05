af9ee14513a6a0acb7be716fb823f65b00b7ff97
JoaoFerreira123 <100808096+JoaoFerreira123@users.noreply.github.com>
2022-06-04 23:55:26 -0300

    com BUG

--- Day 008/cifra de cesar.py
+++ Day 008/cifra de cesar.py
@@ -1,1 +1,21 @@
 alfabeto = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+opcao = int(input('Quer ENCRIPTAR(1) ou DESENCRIPTAR?(2) '))
+codigo = int(input('Qual o código? '))
+msg = input('Digite a mensagem: ').lower()
+encriptada = []
+desencripta = []
+if opcao == 1:
+    for i in range(len(alfabeto)):
+        for j in range(len(msg)):
+            if alfabeto[i] == msg[j]:
+                encriptada.append(alfabeto[i+codigo])
+
+if opcao == 2:
+    for i in range(len(alfabeto)):
+        for j in range(len(msg)):
+            if alfabeto[i] == msg[j]:
+                desencripta.append(alfabeto[i-codigo])
+
+print(''.join(encriptada))
+print(''.join(desencripta))
