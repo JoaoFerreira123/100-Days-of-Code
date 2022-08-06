1bad03530bbe4d6caa20807271e60b2e97341978
JoaoFerreira123 <100808096+JoaoFerreira123@users.noreply.github.com>
2022-08-06 00:16:34 -0300

    ok

--- Day 025 - CSV Data and Pandas Library/main.py
+++ Day 025 - CSV Data and Pandas Library/main.py
@@ -6,30 +6,25 @@ s.setup(700, 700)
 s.title('Jogo dos Estados')
 imagem = 'Day 025 - CSV Data and Pandas Library\mapaBrasil.gif'
 s.addshape(imagem)
-
 b = Turtle(imagem)
 
+#Pega os dados de X e Y das posições dos estados
 dados = pandas.read_csv('Day 025 - CSV Data and Pandas Library\estados.csv')
-
 corX = dados['x']
 corY = dados['y']
 
-pontos = []
+#Cria os pontos de teste das posições
+#dots = []
+#for i in range(len(corX)):
+#    x = Turtle('circle')
+#    x.up()
+#    x.goto(corX[i], corY[i])
+#    dots.append(x)
 
-for i in range(len(corX)):
-    x = Turtle('circle')
-    x.up()
-    
-    x.goto(corX[i], corY[i])
-    pontos.append(x)
-
-
-
-print(corX)
-print(corY)
+#Pop-up entrada de dados
+estado = s.textinput('Adivinhe o Estado', 'Qual Estado é o Próximo? ')
 
 
+#Usado pra não sair da tela quando clicar
 s.mainloop()
-
-print()
 
