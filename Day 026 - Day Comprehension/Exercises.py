2a8051122cc774880d03ae220828ac8473d77cf5
JoaoFerreira123 <100808096+JoaoFerreira123@users.noreply.github.com>
2022-08-06 19:26:31 -0300

    ok

---
+++ Day 026 - Day Comprehension/Exercises.py
@@ -0,0 +1,15 @@
+#Eleve ao quadrado os números da lista usando List Comprehension
+numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+quadrado = [n**2 for n in numbers]
+print(quadrado)
+
+#Filtre os números pares
+numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+pares = [i for i in numbers if i%2 == 0]
+print(pares)
+
+#Cria uma lista que possui os elementos comuns a duas listas
+lista1 = [3,6,5,8,33,12,7,4,72,2,42,13]
+lista2 = [3,6,13,5,7,89,12,3,33,34,1,344,42]
+comum = [int(num) for num in lista1 if num in lista2]
+print(comum)
