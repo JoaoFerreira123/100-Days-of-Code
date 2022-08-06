043be3cd693d6225f8475999ec3470727a632327
JoaoFerreira123 <100808096+JoaoFerreira123@users.noreply.github.com>
2022-08-06 00:10:30 -0300

    ok

--- Day 025 - CSV Data and Pandas Library/main.py
+++ Day 025 - CSV Data and Pandas Library/main.py
@@ -1,4 +1,5 @@
 from turtle import *
+import pandas
 
 s = Screen()
 s.setup(700, 700)
@@ -7,11 +8,28 @@ imagem = 'Day 025 - CSV Data and Pandas Library\mapaBrasil.gif'
 s.addshape(imagem)
 
 b = Turtle(imagem)
-def getMouseXY(x, y):
-    print(x, y)
 
-s.onscreenclick(getMouseXY)
+dados = pandas.read_csv('Day 025 - CSV Data and Pandas Library\estados.csv')
+
+corX = dados['x']
+corY = dados['y']
+
+pontos = []
+
+for i in range(len(corX)):
+    x = Turtle('circle')
+    x.up()
+    
+    x.goto(corX[i], corY[i])
+    pontos.append(x)
+
+
+
+print(corX)
+print(corY)
+
 
 s.mainloop()
 
+print()
 
