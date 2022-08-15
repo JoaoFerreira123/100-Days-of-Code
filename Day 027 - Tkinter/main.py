e5ba610a2d63556ad9b95392ae057567668f93dd
JoaoFerreira123 <100808096+JoaoFerreira123@users.noreply.github.com>
2022-08-14 22:19:12 -0300

    ok

--- Day 027 - Tkinter/main.py
+++ Day 027 - Tkinter/main.py
@@ -1,12 +1,26 @@
-import tkinter 
+from __future__ import with_statement
+from struct import pack
+from tkinter import * 
 
-janela = tkinter.Tk()
+janela = Tk()
 janela.minsize(500, 300)
 janela.title('Primeiro GUI')
 
-camada = tkinter.Label(text='Olá, mundo',  font=('Arial', 20, 'bold'))
+camada = Label(text='Olá, mundo',  font=('Arial', 20, 'bold'))
 camada.pack()
 
+#Botões
+def clique():
+    #quando clicar no botão, o texto de entrada vai aparecer no label
+    camada['text'] = entrada.get()
+
+
+botao = Button(text='Click Me', command=clique)
+botao.pack()
+
+#Entry
+entrada = Entry(width=10)
+entrada.pack()
 
 
 janela.mainloop()
