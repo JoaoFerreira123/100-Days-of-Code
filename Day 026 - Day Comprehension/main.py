cbf610c039b88948854a33e8b553442ffa52736d
JoaoFerreira123 <100808096+JoaoFerreira123@users.noreply.github.com>
2022-09-10 12:16:06 -0300

    ok

--- Day 026 - Day Comprehension/main.py
+++ Day 026 - Day Comprehension/main.py
@@ -7,6 +7,17 @@ import pandas
 dados = pandas.read_csv('Day 026 - Day Comprehension/nato_phonetic_alphabet.csv')
 alf_fonetico = {linha.letra:linha.codigo for (codigo, linha) in dados.iterrows()}
 
-palavra = input('Digite uma palavra: ').upper().strip()
-codigos = [alf_fonetico[letra] for letra in palavra]
-print(codigos)
+
+def gerar_fonetico():
+    palavra = input('Digite uma palavra: ').upper().strip()
+    try:
+        codigos = [alf_fonetico[letra] for letra in palavra]
+        
+    except KeyError:
+        print('Apenas Letras do Alfabeto')
+        gerar_fonetico()
+
+    else:
+        print(codigos)
+
+gerar_fonetico()
